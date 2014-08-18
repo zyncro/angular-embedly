5c7fae418807fcae6698922857566ad2d2336733
Sarah Green <sarah.green@LT3585.local>
2014-08-18 13:09:12 -0700

    Added gulp bump and gulp server

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var karma = require('gulp-karma');
 var jshint = require('gulp-jshint');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var bump = require('gulp-bump');
 
 var testFiles = [
   './test/*spec.js'
@@ -38,3 +41,17 @@ gulp.task('watch', function() {
 gulp.task('test', ['lint', 'runTests']);
 
 gulp.task('default', ['watch']);
+
+gulp.task('connect', function() {
+  connect.server();
+});
+
+gulp.task('server', function() {
+  var app = connect().use(serveStatic('.')).listen(8080);
+});
+
+gulp.task('bump', function(){
+  gulp.src('./*.json')
+  .pipe(bump({ type:'minor' }))
+  .pipe(gulp.dest('./'));
+});
